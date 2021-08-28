cf40db01bf545c6fbbbe61271b9a36bc34df70c8
wuting <wuting@kuaishou.com>
2021-08-28 23:03:17 +0800

    text_ie

---
+++ demo/text_ie/trie/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export LANG=zh_CN.UTF-8
+export LANGUAGE=zh_CN:zh:en_US:en
+export PATH=/usr/local/miniconda3/bin/:$PATH
+
+export CUDA_VISIBLE_DEVICES=0
+
+DAVAROCR_PATH=davarocr/
+python $DAVAROCR_PATH/tools/test.py demo/text_ie/trie/configs/wildreceipt_gt_trie.py demo/text_ie/trie/log/wildreceipt_gt_trie/Best_checkpoint.pth --eval macro_f1
